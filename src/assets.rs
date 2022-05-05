eeac77afa46d80e76a37d5ae6a888908dca6257a
Michael Bohn <michael.bohn@freiheit.com>
2022-05-05 07:20:21 +0200

    carmack expand

--- src/assets.rs
+++ src/assets.rs
@@ -328,8 +328,8 @@ pub fn load_map(assets: &Assets, mapnum: usize) -> Result<MapData, String> {
 		let mut reader = util::new_data_reader(&buf);
 		let expanded_len = reader.read_u16();		
 
-		let expanded = carmack_expand(reader.unread_bytes(), expanded_len as usize);
-
+		//let expanded = carmack_expand(reader.unread_bytes(), expanded_len as usize);
+		//println!("expanded map = {}", expanded.len());
 		//TODO carmackexpand
 		//TODO RLEWexpand
 
@@ -351,32 +351,60 @@ pub fn carmack_expand(data: &[u8], len: usize) -> Vec<u8> {
 	let mut in_ptr = 0;
 
 	while length != 0 {
-
-		let count = data[in_ptr];
+		let word_count = data[in_ptr]; 
 		let ch_high = data[in_ptr+1];
 		in_ptr += 2;
 
 		if ch_high == NEARTAG {
-			let offset = data[in_ptr+1];
+			let offset = data[in_ptr];
 			in_ptr += 1;
 
-			if count == 0 {
+			if word_count == 0 {
 				expanded.push(offset);
+				expanded.push(NEARTAG);
 				length -= 1;
 			} else {
-				let mut copy_ptr = expanded.len() - offset as usize;
-				length -= count as usize;
-				for _ in 0..count {
+				let mut copy_ptr = expanded.len() - (offset as usize * 2);
+				length -= word_count as usize;
+				for _ in 0..(word_count as usize * 2) {
+					expanded.push(expanded[copy_ptr]);
+					copy_ptr += 1;
+				}
+			}
+		} else if ch_high == FARTAG {
+			let offset_low = data[in_ptr];
+			in_ptr += 1;
+
+			if word_count == 0 {
+				expanded.push(offset_low);
+				expanded.push(FARTAG);
+				length -= 1;
+			} else {
+				let offset_high = data[in_ptr];
+				in_ptr += 1;
+
+				let mut offset = offset_low as usize;
+				offset  <<= 8;
+				offset |= offset_high as usize;
+
+				let mut copy_ptr = (offset-1) * 2; 
+				length -= word_count as usize;
+				for _ in 0..(word_count as usize * 2) {
 					expanded.push(expanded[copy_ptr]);
 					copy_ptr += 1;
 				}
 			}
 		} else {
-			//add word as is (destructured here as count and ch_high)
-			expanded.push(count);
+			// add word as is (destructured here as count and ch_high)
+			expanded.push(word_count);
 			expanded.push(ch_high);
 			length -= 1;
 		}
+	}
+
+	// handle trailing byte at the end if len is odd
+	if expanded.len() != len {
+		expanded.push(data[in_ptr]);
 	}
 
 	expanded
